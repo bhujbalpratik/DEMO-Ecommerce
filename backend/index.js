3f405a8a7dc8a8bbd6d624a85a03c894fe20969e
bhujbal pratik <pratikbhujbal743@gmail.com>
2024-03-16 15:00:07 +0530

    React and tailwind css added to project

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,19 @@ import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
-dotenv.config()
+dotenv.config({ path: "./backend/config/.env" })
 
 connectDB()
+
+const port = process.env.PORT
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.get("/", (req, res) => res.send("hello,Everything is working "))
+
+app.listen(port, () =>
+  console.log(`server is working on http://localhost:${port}`)
+)
