745be93fa1571864d0e4c4a19a29e9e7ff4050fb
bhujbal pratik <pratikbhujbal743@gmail.com>
2024-03-16 07:59:45 +0530

    .gitignore Added

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-export const connectDB = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, { dbName: "MERNStore" })
-    .then(() => console.log(`Database connected 👍`))
-    .catch((e) => console.log(`Error while database connection : ${e}`))
-}
